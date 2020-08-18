72cf0d801ba3cc9b7d627b269b65a283681504bc
watfome <watfome@gmail.com>
2020-08-18 17:02:43 +0300

    solved

--- codeforces_50A.py
+++ codeforces_50A.py
@@ -1,2 +1,33 @@
-x = list(map(int, input().split()))
-print(x)
+# Get two ints m and n for height and length
+m_height, n_length = list(map(int, input().split()))
+
+if m_height < 1 or m_height > n_length or n_length > 16:
+    print("Wrong input")
+    exit()
+
+count_dominos = 0
+is_vertical = False
+
+for row in range(m_height):
+    temp_n = n_length
+    while True:
+        if temp_n - 2 >= 0:
+            temp_n -= 2
+            count_dominos += 1
+            # print(f"row is {row} and n_temp is {temp_n} and using branch 1")
+        elif temp_n - 2 == -1 and m_height - row >= 2 and not is_vertical:
+            temp_n -= 2
+            count_dominos += 1
+            is_vertical = True
+            # print(f"row is {row} and n_temp is {temp_n} and using branch 2")
+            break
+        elif temp_n - 2 == -1 and is_vertical:
+            temp_n -= 2
+            is_vertical = False
+            # print(f"row is {row} and n_temp is {temp_n} and using branch 3")
+            break
+        else:
+            # print(f"row is {row} and n_temp is {temp_n} and using branch 4")
+            break
+print(count_dominos)
+
