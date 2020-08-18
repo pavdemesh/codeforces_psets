f894aaa319522c911d97cd0d78de39608c3d6bec
watfome <watfome@gmail.com>
2020-08-18 17:53:32 +0300

    revised

--- codeforces_50A.py
+++ codeforces_50A.py
@@ -11,17 +11,17 @@ is_vertical = False
 for row in range(m_height):
     temp_n = n_length
     while True:
-        if temp_n - 2 >= 0:
+        if temp_n >= 2:
             temp_n -= 2
             count_dominos += 1
             # print(f"row is {row} and n_temp is {temp_n} and using branch 1")
-        elif temp_n - 2 == -1 and m_height - row >= 2 and not is_vertical:
+        elif temp_n == 1 and m_height - row >= 2 and not is_vertical:
             temp_n -= 2
             count_dominos += 1
             is_vertical = True
             # print(f"row is {row} and n_temp is {temp_n} and using branch 2")
             break
-        elif temp_n - 2 == -1 and is_vertical:
+        elif temp_n == 1 and is_vertical:
             temp_n -= 2
             is_vertical = False
             # print(f"row is {row} and n_temp is {temp_n} and using branch 3")
